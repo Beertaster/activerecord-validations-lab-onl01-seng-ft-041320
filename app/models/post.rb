62e5d2d246d2d8adc000f7c6af9f844d9424ef70
William Stevens <wstevens.hornet@gmail.com>
2020-07-03 00:36:15 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-    validates :content, length: {minimum: 250}
-    validates :summary, length: {maximum: 250}
-    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
-    validate :is_clickbait?
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :is_clickbait?
 
     CLICKBAIT_PATTERNS = [
         /Won't Believe/i,
